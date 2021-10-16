c483ac3db502a29608dc716501d52caab7370ab9
rabosian <msann1356@gmail.com>
2021-10-16 19:42:50 -0400

    Select Dates feature added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, {useState} from 'react';
 import './App.css';
-import "react-date-range/dist/styles.css";
-import 'react-date-range/dist/theme/default.css';
 import Sidebar from './components/Sidebar';
-import Calendar from "./components/Calendar"
+import { Calendar } from 'react-modern-calendar-datepicker';
+import "react-modern-calendar-datepicker/lib/DatePicker.css";
 
 function App() {
+  const [selectedDay, setSelectedDay] = useState([]);
 
   return (
     <div className="App">
       <Sidebar />
-      <Calendar />
+      <h3>Daily Check-in
+        <br/> 
+      </h3>
+      <p>Good 😊 Normal 🙂 Sad 🤢</p> <hr />
+      <Calendar
+        value={selectedDay}
+        onChange={setSelectedDay}
+        calendarClassName="myCalendar"
+        calendarTodayClassName="today"
+        shouldHighlightWeekends
+      />
     </div>
   );
 }
