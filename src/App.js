d05b1cfe5352e312f69999d88a125dcaee64c66c
rabosian <msann1356@gmail.com>
2021-10-16 22:47:07 -0400

    Added multi pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, {useState} from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
-import Sidebar from './components/Sidebar';
-import { Calendar } from 'react-modern-calendar-datepicker';
 import "react-modern-calendar-datepicker/lib/DatePicker.css";
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom"
+import MainPage from './pages';
+import NotFoundPage from './pages/404';
+import ProfilePage from './pages/profile';
 
-function App() {
-  const [selectedDay, setSelectedDay] = useState([]);
-
-  return (
-    <div className="App">
-      <Sidebar />
-      <h3>Daily Check-in
-        <br/> 
-      </h3>
-      <p>Good 😊 Normal 🙂 Sad 🤢</p> <hr />
-      <Calendar
-        value={selectedDay}
-        onChange={setSelectedDay}
-        calendarClassName="myCalendar"
-        calendarTodayClassName="today"
-        shouldHighlightWeekends
-      />
-    </div>
-  );
+class App extends Component  {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={MainPage} />
+            <Route exact path="/home" component={MainPage} />
+            <Route exact path="/profile" component={ProfilePage} />
+            <Route exact path="/404" component={NotFoundPage} />
+            <Redirect to="/404"/>
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
