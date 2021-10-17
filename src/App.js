d90de7b4e44d794e96e6acdbce8dd7ca8ab9a7bf
Michelle <michelleyu230@gmail.com>
2021-10-16 23:30:04 -0400

    resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
-import Signup from './components/SignUp/Signup';
 import Home from './components/pages/HomePage/Home';
 import Signin from './components/Signin/Signin';
 import Login from './components/pages/Login';
 import React, { Component, useState } from 'react';
-import './App.css';
 import "react-modern-calendar-datepicker/lib/DatePicker.css";
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom"
 import MainPage from './pages';
@@ -20,6 +17,9 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
+            <Route path='/' exact component={Login} />
+            <Route path='/home' component={Home} />
+            <Route path='/sign-in' component={Signin} />
             <Route exact path="/calendar" component={MainPage} />
             <Route exact path="/profile" component={ProfilePage} />
             <Route exact path="/404" component={NotFoundPage} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,7 +41,7 @@ function Navbar() {
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                            <Link to="/calendar" className="nav-links" onClick={closeMobileMenu}>
                                 Calendar
                             </Link>
                         </li>
