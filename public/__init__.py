c7335adcc0c943aeb45e46ee3f96f5196d86a575
Gabriel Chan <gabriel.chanyk@gmail.com>
2021-10-17 09:51:41 -0400

    flask files

--- public/__init__.py
+++ public/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from os import path
 
+
+DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret'
+    from .views import views
+    from .auth import auth
 
-    return 
+    app.register_blueprint(views, url_prefix = '/')
+    app.register_blueprint(auth, url_prefix = '/')
+    return app
